47b200d9251c3e6880a699cf30d89dbb6808e2b3
KarynaSerebrianska <karinaserebryanskaya7@gmail.com>
2025-03-17 22:08:26 +0200

    second

--- second.py
+++ second.py
@@ -1,16 +1,16 @@
 import random
 
 def get_numbers_ticket(min_value, max_value, quantity):
-    # Валідація вхідних параметрів
+    
     if not (1 <= min_value <= max_value <= 1000) or quantity > (max_value - min_value + 1):
-        return []  # Повертаємо порожній список, якщо параметри некоректні
+        return []  
 
     # Генеруємо унікальний набір чисел
     numbers = random.sample(range(min_value, max_value + 1), quantity)
 
-    # Повертаємо відсортований список
+    
     return sorted(numbers)
 
-# Приклад використання:
+
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
